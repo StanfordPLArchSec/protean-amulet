dd326999cf4dc1dbe2acd9c0c92f0d7bc7bbe627
Nicholas Mosier <nmosier@stanford.edu>
2025-12-08 17:17:05 -0700

    initial commit

---
+++ helpers/host-runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("log", nargs="+")
+args = parser.parse_args()
+
+def get_seconds(path):
+    l = []
+    with open(path) as f:
+        for line in f:
+            if m := re.fullmatch(r"Duration Elapsed: (\d+)hrs-(\d+)mins-(\d+)secs", line.strip()):
+                s = int(m.group(3)) + int(m.group(2)) * 60 + int(m.group(1)) * 60 * 60
+                l.append(s)
+    if len(l) == 0:
+        print(f"ERROR: missing duration: {path}", file=sys.stderr)
+        exit(1)
+    return l[-1]
+
+x = sum(map(get_seconds, args.log))
+print(x)
